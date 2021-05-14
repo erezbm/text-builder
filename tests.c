9646f6fa49fe989055a38c5d82e5a4fa76ac51c2
Erez Ben Moshe <erezbmp@gmail.com>
2021-05-14 09:18:14 +0300

    Add tests for append & prepend

--- tests.c
+++ tests.c
@@ -4,25 +4,39 @@
 
 #include "src/builders.h"
 
-#define TEST_BUILDER(builder, input, expected) {\
+#define TEST_BUILDER(builder, expected, input) {\
 	char *text = strdup(input);\
 	builder(&text);\
-	if (strcmp(text, expected) != 0) {\
-		printf(#builder "\n");\
-		printf("actual   = %s\n", text);\
-		printf("expected = %s\n", expected);\
-		exit_status = 1;\
-	}\
+	exit_status |= expect_str(expected, text, #builder ":");\
 	free(text);\
+}
+
+#define TEST_BUILDER_WITH_ARGS(builder, expected, input, ...) {\
+	char *text = strdup(input);\
+	builder(&text, __VA_ARGS__);\
+	exit_status |= expect_str(expected, text, #builder ":");\
+	free(text);\
+}
+
+int expect_str(char *expected, char *actual, char *fail_message) {
+	if (strcmp(actual, expected) != 0) {
+		puts(fail_message);
+		printf("expected = %s\n", expected);
+		printf("actual   = %s\n", actual);
+		return 1;
+	}
+	return 0;
 }
 
 int main() {
 	int exit_status = 0;
 
 	/* Builders Tests */
-	TEST_BUILDER(uppercase, "1aAaA1", "1AAAA1");
-	TEST_BUILDER(lowercase, "1aAaA1", "1aaaa1");
-	TEST_BUILDER(reverse, "1aAaA1", "1AaAa1");
+	TEST_BUILDER(uppercase, "1AAAA1", "1aAaA1");
+	TEST_BUILDER(lowercase, "1aaaa1", "1aAaA1");
+	TEST_BUILDER(reverse, "1AaAa1", "1aAaA1");
+	TEST_BUILDER_WITH_ARGS(append, "hello world", "hello", " world");
+	TEST_BUILDER_WITH_ARGS(prepend, "hello world", "world", "hello ");
 
 	if (exit_status == 0)
 		puts("All tests succeeded");
