d1199823b61e74bfb994b9cd38900c635b46fdf0
Erez Ben Moshe <erezbmp@gmail.com>
2021-05-15 04:10:40 +0300

    Add usage and help messages

--- src/main.c
+++ src/main.c
@@ -6,11 +6,32 @@
 #include "utils.h"
 #include "buildtext.h"
 
+void print_usage(char *name) {
+	printf("Usage: %s [-ulrR] [-a postfix] [-p prefix] [-m count]\n", name);
+}
+
+void print_options() {
+	puts("  -u uppercase the input");
+	puts("  -l lowercase the input");
+	puts("  -r reverse the input");
+	puts("  -R randomcase the input");
+	puts("  -a append <postfix> at the end of the input");
+	puts("  -p prepend <prefix> at the start of the input");
+	puts("  -m repeat the input <count> times (negative <count> means 0)");
+}
+
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
+	opterr = 0;
 	int opt;
-	while ((opt = getopt(argc, argv, "ulRra:p:m:")) != -1)
-		if (opt == '?')	return 1;
+	while ((opt = getopt(argc, argv, "ulRra:p:m:")) != -1) {
+		if (opt == '?')	{
+			print_usage(argv[0]);
+			if (optopt == 'h')
+				print_options();
+			return optopt != 'h';
+		}
+	}
 
 	char *line = NULL;
 	size_t len = 0;
